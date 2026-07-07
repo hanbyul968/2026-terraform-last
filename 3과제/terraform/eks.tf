7a3f8335f20944e81e312a617e1c78b128c69d94
hnmly <janghanbyul2009@gmail.com>
2026-07-07 10:30:44 +0900

    fix: add ingress rule to cluster SG for node join (NodeCreationFailure)

--- 3과제/terraform/eks.tf
+++ 3과제/terraform/eks.tf
@@ -20,12 +20,30 @@ resource "aws_security_group" "eks_cluster" {
   description = "EKS control plane SG"
   vpc_id      = aws_vpc.this.id
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Public API access"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group_rule" "cluster_from_nodes" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_node.id
+  description              = "Nodes to cluster API"
 }
 
 resource "aws_security_group" "eks_node" {
